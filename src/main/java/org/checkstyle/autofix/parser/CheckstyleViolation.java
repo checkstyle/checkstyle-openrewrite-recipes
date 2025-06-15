76f932bd12337034676abf5920c19e32e3042632
Anmol202005 <anmol20102005@gmail.com>
2025-06-15 23:01:52 +0530

    Implemented report and config parser

---
+++ src/main/java/org/checkstyle/autofix/parser/CheckstyleViolation.java
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle-openrewrite-recipes: Automatically fix Checkstyle violations with OpenRewrite.
+// Copyright (C) 2025 The Checkstyle OpenRewrite Recipes Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package org.checkstyle.autofix.parser;
+
+public final class CheckstyleViolation {
+
+    private final Integer line;
+
+    private final Integer column;
+
+    private final String severity;
+
+    private final String source;
+
+    private final String message;
+
+    private final String fileName;
+
+    public CheckstyleViolation(Integer line, Integer column,
+                               String severity, String source, String message, String fileName) {
+        this.line = line;
+        this.column = column;
+        this.severity = severity;
+        this.source = source;
+        this.message = message;
+        this.fileName = fileName;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+}
