4100debaf59708c1be3b76923aff9acd1f72cf3e
Anmol202005 <anmol20102005@gmail.com>
2025-07-25 22:42:45 +0530

    Issue #41: add violation comment violation

--- src/test/resources/org/checkstyle/autofix/recipe/upperell/symbolicliterals/InputSymbolicLiterals.java
+++ src/test/resources/org/checkstyle/autofix/recipe/upperell/symbolicliterals/InputSymbolicLiterals.java
@@ -10,9 +10,9 @@
 package org.checkstyle.autofix.recipe.upperell.symbolicliterals;
 
 public class InputSymbolicLiterals {
-    private long minLong = -9223372036854775808l;
-    private Long negativeLong = -5678l;
-    long a = -0xAl;
-    long b = +-12l;
-    long c = +-(-(-(-4l)));;
+    private long minLong = -9223372036854775808l;    // violation 'Should use uppercase 'L'.'
+    private Long negativeLong = -5678l;              // violation 'Should use uppercase 'L'.'
+    long a = -0xAl;                                  // violation 'Should use uppercase 'L'.'
+    long b = +-12l;                                  // violation 'Should use uppercase 'L'.'
+    long c = +-(-(-(-4l)));;                         // violation 'Should use uppercase 'L'.'
 }
