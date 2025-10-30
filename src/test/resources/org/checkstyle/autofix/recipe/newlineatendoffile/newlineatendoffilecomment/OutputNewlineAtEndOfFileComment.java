cf419b3aa3bf05c704d144dfdea74b95138bee20
Anmol202005 <anmol20102005@gmail.com>
2025-10-30 18:27:06 +0530

    Issue #124: Implement recipe for NewlineAtEndOfFile

---
+++ src/test/resources/org/checkstyle/autofix/recipe/newlineatendoffile/newlineatendoffilecomment/OutputNewlineAtEndOfFileComment.java
@@ -0,0 +1,11 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.NewlineAtEndOfFileCheck
+lineSeparator = LF
+fileExtensions = (default)""
+
+*/
+
+package org.checkstyle.autofix.recipe.newlineatendoffile.newlineatendoffilecomment;
+
+public class OutputNewlineAtEndOfFileComment {
+}     // trailing comment
---
+++ src/test/resources/org/checkstyle/autofix/recipe/newlineatendoffile/newlineatendoffilecrlf/InputNewlineAtEndOfFileCrlf.java
@@ -0,0 +1,12 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.NewlineAtEndOfFileCheck
+lineSeparator = lf
+fileExtensions = (default)""
+
+*/
+// violation 6 lines above 'Expected line ending for file is LF(\\n), but CRLF(\\r\\n) is detected.'
+
+package org.checkstyle.autofix.recipe.newlineatendoffile.newlineatendoffilecrlf;
+
+public class InputNewlineAtEndOfFileCrlf {
+}
