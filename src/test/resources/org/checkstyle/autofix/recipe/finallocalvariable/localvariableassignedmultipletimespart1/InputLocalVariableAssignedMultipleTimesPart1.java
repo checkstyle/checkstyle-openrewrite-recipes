a149db823e0e0247ba29352f3542a5533e43e9d8
MonuChaudhary14 <monuchaudharypoonia@gmail.com>
2026-05-27 02:46:03 +0530

    Issue #202: Reduced the file length of LocalVariableAssignedMultipleTimes

---
+++ src/test/resources/org/checkstyle/autofix/recipe/finallocalvariable/localvariableassignedmultipletimespart1/InputLocalVariableAssignedMultipleTimesPart1.java
@@ -0,0 +1,99 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="com.puppycrawl.tools.checkstyle.checks.coding.FinalLocalVariableCheck">
+    </module>
+  </module>
+</module>
+
+*/
+package org.checkstyle.autofix.recipe.finallocalvariable.localvariableassignedmultipletimespart1;
+
+public class InputLocalVariableAssignedMultipleTimesPart1 {
+    void foo1() {
+        final boolean some_condition = true;
+        int i;
+        if (some_condition) {
+            i = 1;
+        }
+        i = 2;
+    }
+
+    void foo2() {
+        final boolean some_condition = true;
+        int i;
+        if (some_condition) {
+            i = 1;
+        }
+        else {
+
+        }
+        i = 2;
+    }
+
+    void foo3() {
+        final boolean some_condition = true;
+        int i;
+        if (some_condition) {
+            i = 1;
+            if (i >= 1) {
+
+            }
+            else {
+
+            }
+        }
+        i = 2;
+    }
+
+
+    void foo4() {
+        final boolean some_condition = true;
+        int i; // violation, "Variable 'i' should be declared final"
+        if (some_condition) {
+            if (true) {
+            }
+            else {
+            }
+            i = 1;
+        }
+        else {
+            i = 2;
+        }
+        if (true) {
+
+        }
+        else {
+        }
+
+    }
+
+    void foo5() {
+        final boolean some_condition = true;
+        int i;
+
+        {
+            i = 2;
+        }
+
+        if (some_condition) {
+            i = 1;
+        }
+    }
+
+    void foo6() {
+        final boolean some_condition = true;
+        int i;
+
+        {
+            i = 2;
+        }
+
+        if (some_condition) {
+            i = 1;
+        }
+        else {
+            i = 6;
+        }
+    }
+}
---
+++ src/test/resources/org/checkstyle/autofix/recipe/finallocalvariable/localvariableassignedmultipletimespart4/OutputLocalVariableAssignedMultipleTimesPart4.java
@@ -0,0 +1,110 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="com.puppycrawl.tools.checkstyle.checks.coding.FinalLocalVariableCheck">
+    </module>
+  </module>
+</module>
+
+*/
+package org.checkstyle.autofix.recipe.finallocalvariable.localvariableassignedmultipletimespart4;
+
+public class OutputLocalVariableAssignedMultipleTimesPart4 {
+    public String foo11(final Object o, boolean getMinimal) {
+
+        String n = System.lineSeparator();
+        if (n != null) {
+            return n;
+        }
+
+        try {
+
+            String className = "";
+            int kind = 0;
+            boolean isParameterToInitMethodofAnonymousInnerClass = false;
+            boolean isSyntheticMethod = false;
+
+            if (o instanceof String) {
+
+                className = ((String) o).getClass().getName();
+                kind = 3;
+            }
+            else if (o instanceof String) {
+                assert false;
+                className = (String) o;
+                kind = 4;
+            }
+            else {
+                throw new IllegalArgumentException("Can't" + o.getClass().getName());
+            }
+
+            // <init> method parameters for inner classes don't inherit default
+            // annotations
+            // since some of them are synthetic
+            if (isParameterToInitMethodofAnonymousInnerClass) {
+                return null;
+            }
+
+            // synthetic elements should not inherit default annotations
+            if (isSyntheticMethod) {
+                return null;
+            }
+            try {
+                final String c = new String(className);
+
+                if (c != null && c.equals("")) {
+                    return null;
+                }
+            }
+            catch (Exception e) {
+                assert true;
+            }
+
+            // look for default annotation
+            n = new String(className);
+            if (n == null) {
+                String.CASE_INSENSITIVE_ORDER.equals("Default annotation for " + kind + " is " + n);
+            }
+            if (n != null) {
+                return n;
+            }
+
+            n = new String(className);
+            if (n == null) {
+                String.CASE_INSENSITIVE_ORDER.equals("Default annotation for any is " + n);
+            }
+            if (n != null) {
+                return n;
+            }
+
+            final int p = className.lastIndexOf('.');
+            className = className.substring(0, p + 1) + "package-info";
+            n = new String(className);
+            if (n == null) {
+                String.CASE_INSENSITIVE_ORDER.equals("Default annotation for " + kind + " is " + n);
+            }
+            if (n != null) {
+                return n;
+            }
+
+            n = new String(className);
+            if (n == null) {
+                String.CASE_INSENSITIVE_ORDER.equals("Default annotation for any is " + n);
+            }
+            if (n != null) {
+                return n;
+            }
+
+            isParameterToInitMethodofAnonymousInnerClass = true;
+            isSyntheticMethod = true;
+
+            return n;
+        }
+        catch (Exception e) {
+            String.CASE_INSENSITIVE_ORDER.equals(e);
+            ;
+            return null;
+        }
+
+    }
+}
