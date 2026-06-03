c90b88f2e3d445d36cda137d3c29fea151b9ae19
MonuChaudhary14 <monuchaudharypoonia@gmail.com>
2026-06-03 12:36:17 +0530

    Issue #221: Fix the RemoveViolationComments bug for TrailingComma marker

--- src/test/java/org/checkstyle/autofix/RemoveViolationComments.java
+++ src/test/java/org/checkstyle/autofix/RemoveViolationComments.java
@@ -24,9 +24,12 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.marker.TrailingComma;
 import org.openrewrite.java.tree.Comment;
 import org.openrewrite.java.tree.Space;
 import org.openrewrite.java.tree.TextComment;
+import org.openrewrite.marker.Marker;
+import org.openrewrite.marker.Markers;
 
 public class RemoveViolationComments extends Recipe {
 
@@ -50,6 +53,41 @@ public class RemoveViolationComments extends Recipe {
         @Override
         public Space visitSpace(
                 Space space, Space.Location loc, ExecutionContext executionContext) {
+            final Space result = removeViolationCommentsFromSpace(space);
+            return super.visitSpace(result, loc, executionContext);
+        }
+
+        @Override
+        public Markers visitMarkers(
+            Markers markers, ExecutionContext executionContext) {
+            final Markers visitedMarkers =
+                super.visitMarkers(markers, executionContext);
+
+            boolean changed = false;
+            final List<Marker> newMarkers =
+                new ArrayList<>(visitedMarkers.getMarkers().size());
+
+            for (Marker marker : visitedMarkers.getMarkers()) {
+                if (marker instanceof TrailingComma trailingComma) {
+                    final Space newSuffix =
+                        removeViolationCommentsFromSpace(trailingComma.getSuffix());
+                    if (newSuffix != trailingComma.getSuffix()) {
+                        newMarkers.add(trailingComma.withSuffix(newSuffix));
+                        changed = true;
+                        continue;
+                    }
+                }
+                newMarkers.add(marker);
+            }
+
+            Markers result = visitedMarkers;
+            if (changed) {
+                result = visitedMarkers.withMarkers(newMarkers);
+            }
+            return result;
+        }
+
+        private Space removeViolationCommentsFromSpace(Space space) {
             String suffixAccumulator = null;
             final List<Comment> filteredComments = new ArrayList<>();
 
@@ -89,7 +127,7 @@ public class RemoveViolationComments extends Recipe {
                             mergeWhitespace(result.getWhitespace(), suffixAccumulator));
                 }
             }
-            return super.visitSpace(result, loc, executionContext);
+            return result;
         }
 
         private String mergeWhitespace(String original, String suffix) {
